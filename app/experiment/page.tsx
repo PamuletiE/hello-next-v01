2d1b9f141bdd79f5d892ec1e92e4fb4bf924b2e2
PamuletiE <pamuleti.ed@gmail.com>
2026-03-16 07:26:02 +0530

    Added link to go back

--- app/experiment/page.tsx
+++ app/experiment/page.tsx
@@ -3,12 +3,14 @@ import { Switch } from "@/components/ui/switch";
 import { Sun } from "lucide-react";
 import { Moon } from "lucide-react";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function ExperimentPage() {
 
     const { theme, setTheme } = useTheme()
 
     return (
+        <>
         <div className="absolute top-6 right-6 flex items-center gap-3">
         <Sun
           className="h-5 w-5 transition-colors"
@@ -21,5 +23,10 @@ export default function ExperimentPage() {
           className="h-5 w-5 transition-colors"
         />
       </div>
+      <Link
+      href="/">
+        Back to Home
+        </Link>
+      </>
     )
 }
